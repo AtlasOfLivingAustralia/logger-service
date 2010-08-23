724b2dbafd1ec7594ec43d72f5ff583ff1af0e9b
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-08-23 04:59:42 +0000

    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-logger-service@1652 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/test/java/org/ala/dao/LogEventDaoTest.java
+++ src/test/java/org/ala/dao/LogEventDaoTest.java
@@ -32,44 +32,39 @@ public class LogEventDaoTest {
      */
     @Before
     public void before() throws Exception {
-    	LogDetail logDetail = new LogDetail();
-    	logDetail.setEntityUid("mok");
-    	logDetail.setEntityType("a");
-    	logDetail.setRecordCount(1);
-    	
-    	LogEvent entity1 = new LogEvent();
-    	entity1.setCreated(new Date());
-    	entity1.setComment("World");
-    	entity1.setUserEmail("waiman.mok@csiro.au");
-    	entity1.setUserIp("123.12.12.123");
-                
-        HashSet<LogDetail> set = new HashSet<LogDetail>();
-        set.add(logDetail);
-        entity1.setLogDetails(set);
-        logEventDao.save(entity1);        
+//    	LogDetail logDetail = new LogDetail();
+//    	logDetail.setEntityUid("mok");
+//    	logDetail.setEntityType("a");
+//    	logDetail.setRecordCount(1);
+//    	
+//    	LogEvent entity1 = new LogEvent();
+//    	entity1.setCreated(new Date());
+//    	entity1.setComment("World");
+//    	entity1.setUserEmail("waiman.mok@csiro.au");
+//    	entity1.setUserIp("123.12.12.123");
+//                
+//        HashSet<LogDetail> set = new HashSet<LogDetail>();
+//        set.add(logDetail);
+//        entity1.setLogDetails(set);
+//        logEventDao.save(entity1);        
     }
     
     @Test
     public void testTemplate() throws SQLException {
-    	Collection<LogEvent> l = logEventDao.findLogEvents();
-    	for(LogEvent e : l){
-    		System.out.println("LogEvent: " + e.toString());
-    		Set<LogDetail> s = e.getLogDetails();
-    		for(LogDetail d : s){
-    			System.out.println("LogDetail: " + d.toString());
-    		}
-    	}
+    	Collection<LogEvent> l = logEventDao.findLogEvents(1, 10);
     	
     	LogEvent event = logEventDao.findLogEventById(1);
     	System.out.println(event.toString());
     	
-    	l = logEventDao.findLogEventsByEmail("waiman.mok@csiro.au");
+    	l = logEventDao.findLogEventsByEmail("dave.martin@csiro.au");
+    	
+    	l = logEventDao.findLogEventsByUserIp("127.0.0.1");
     	for(LogEvent e : l){
     		System.out.println("LogEvent: " + e.toString());
-    		Set<LogDetail> s = e.getLogDetails();
-    		for(LogDetail d : s){
-    			System.out.println("LogDetail: " + d.toString());
-    		}
+//    		Set<LogDetail> s = e.getLogDetails();
+//    		for(LogDetail d : s){
+//    			System.out.println("LogDetail: " + d.toString());
+//    		}
     	}
     	
     	LogDetail logDetail = new LogDetail();
