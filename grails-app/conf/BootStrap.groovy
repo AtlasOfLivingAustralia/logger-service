14e3e9bd12e0b590cce30beb97402be0858dfc9b
m-r-c <m-r-c@users.noreply.github.com>
2014-12-11 14:32:38 +1100

    Create RemoteAddress for localhost on startup

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,11 @@
+import au.org.ala.logger.RemoteAddress
+
 class BootStrap {
 
     def init = { servletContext ->
-        
+        if (!RemoteAddress.findByIp("127.0.0.1")) {
+            new RemoteAddress(ip: "127.0.0.1", hostName: "localhost").save(flush: true)
+        }        
     }
 
     def destroy = {
