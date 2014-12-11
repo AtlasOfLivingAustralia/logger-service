bf358a4a93e8bcdda78d19ceb845cd7909f9c58a
m-r-c <m-r-c@users.noreply.github.com>
2014-12-11 14:55:54 +1100

    Reverted change to create RemoteAddress

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,12 +1,7 @@
-import au.org.ala.logger.RemoteAddress
-
 class BootStrap {
 
     def init = { servletContext ->
-        if (!RemoteAddress.findByIp("127.0.0.1")) {
-            new RemoteAddress(ip: "127.0.0.1", hostName: "localhost").save(flush: true)
-        }        
-    }
+    }        
 
     def destroy = {
     }
