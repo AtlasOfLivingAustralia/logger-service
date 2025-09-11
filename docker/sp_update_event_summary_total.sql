ef0f6f86643509104e49e9ba1361b2d942a91267
QBai <31238531+qifeng-bai@users.noreply.github.com>
2025-09-11 16:39:53 +1000

    #78 fix count of event for event_summary_total table

--- docker/sp_update_event_summary_total.sql
+++ docker/sp_update_event_summary_total.sql
@@ -5,45 +5,105 @@ CREATE DEFINER=`logger`@`%` PROCEDURE `batch_process_event_summary_totals`(
 )
 BEGIN
     DECLARE done INT DEFAULT 0;
+    DECLARE total_log_events_to_process BIGINT DEFAULT 0;
     -- v_ variables are written into the temporary table
     DECLARE v_month INT;
     DECLARE v_event_type_id INT;
     DECLARE v_entity_prefix VARCHAR(2); -- e.g., 'dr', 'co', etc.
-    DECLARE v_num_events INT;
-    DECLARE v_total_records INT;
+    DECLARE v_num_events BIGINT;
+    DECLARE v_total_records BIGINT;
 
     -- output variables for debugging
-    DECLARE current_number_of_events INT DEFAULT 0;
-    DECLARE current_record_count INT DEFAULT 0;
-    DECLARE updated_number_of_events INT DEFAULT 0;
-    DECLARE updated_record_count INT DEFAULT 0;
+--     DECLARE current_number_of_events INT DEFAULT 0;
+--     DECLARE current_record_count INT DEFAULT 0;
+--     DECLARE updated_number_of_events INT DEFAULT 0;
+--     DECLARE updated_record_count INT DEFAULT 0;
 
-    -- Declare cursor for iterating over a temporary database stored the aggregated results
+    -- Declare cursor for iterating over a temporary database stored the aggregated results for total_records of 'dr' entity only
     DECLARE cur CURSOR FOR
-		SELECT month, log_event_type_id,entity_prefix, num_log_details, total_record_count
+		SELECT month, log_event_type_id,entity_prefix, num_log_event, total_record_count
 		FROM tmp_aggregated_results;
+    DECLARE cur_summary CURSOR FOR
+        SELECT month, log_event_type_id, num_log_event FROM tmp_log_event_summary;
 
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-
-    SELECT "DEBUG: event_summary_breakdown_total", p_start_id, p_end_id;
     -- Drop temporary table if it already exists
     DROP TEMPORARY TABLE IF EXISTS tmp_aggregated_results;
+    DROP TEMPORARY TABLE IF EXISTS tmp_log_event_summary;
 
-    -- 2. Create temporary table with
-    -- aggregated counts of log details and sum of total_records per event type and month
+    -- START: Count log_event (excludes those without log_details) based on month and event type(e.g 1002)
+    CREATE TEMPORARY TABLE tmp_log_event_summary (
+        month INT,
+        log_event_type_id INT,
+        num_log_event INT
+    );
+
+   INSERT INTO tmp_log_event_summary (month, log_event_type_id, num_log_event)
+       SELECT
+            le.month AS month,
+            le.log_event_type_id AS log_event_type_id,
+            COUNT(le.id) AS num_log_event
+        FROM log_event le
+        WHERE le.id >= p_start_id
+          AND le.id <= p_end_id
+          AND EXISTS (
+            SELECT 1
+            FROM log_detail ld
+            WHERE ld.log_event_id = le.id
+            )
+        GROUP BY le.month, le.log_event_type_id
+        ORDER BY le.log_event_type_id, le.month;
+
+    OPEN cur_summary;
+
+    read_loop: LOOP
+             SET done = 0;
+             FETCH cur_summary INTO v_month, v_event_type_id, v_num_events;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+             IF EXISTS (
+                    SELECT 1
+                    FROM event_summary_totals
+                    WHERE month = v_month
+                      AND log_event_type_id = v_event_type_id
+                )
+            THEN
+                UPDATE event_summary_totals
+                SET number_of_events = number_of_events + v_num_events
+                WHERE month = v_month
+                  AND log_event_type_id = v_event_type_id;
+            ELSE
+                INSERT INTO event_summary_totals (
+                    month, log_event_type_id, number_of_events, record_count
+                )
+                VALUES (v_month, v_event_type_id, v_num_events,0);
+            END IF;
+
+    END LOOP;
+    CLOSE cur_summary;
+    -- END: Count log_event (excludes those without log_details) based on month and event type(e.g 1002)
+
+    -- 2. START
+    -- sum of total_records ('dr entity type only') per event type and month
     -- for log events in the specified ID range
     CREATE TEMPORARY TABLE tmp_aggregated_results AS
 		SELECT
 			le.month AS month,
 						le.log_event_type_id AS log_event_type_id,
 						LEFT(ld.entity_uid, 2) AS entity_prefix,
-						COUNT(ld.id) AS num_log_details,
+						COUNT(le.id) AS num_log_event,
 						COALESCE(SUM(ld.record_count), 0) AS total_record_count
 		FROM log_event le
 			LEFT JOIN log_detail ld
 		ON ld.log_event_id = le.id
 		WHERE le.id >= p_start_id
 		  AND le.id <= p_end_id
+          AND EXISTS (
+            SELECT 1
+            FROM log_detail ld
+            WHERE ld.log_event_id = le.id
+            )
 		GROUP BY le.month, le.log_event_type_id, entity_prefix
 		ORDER BY le.log_event_type_id, le.month;
 
@@ -65,25 +125,6 @@ BEGIN
 						--                             UPDATE event_summary_totals est SET record_count = record_count + NEW.record_count
 						--                      WHERE est.month = new_month AND est.log_event_type_id = new_log_event_type_id;
 						--                        END IF;
-
-					-- Update the number of event to the summary table
-					IF EXISTS (
-							SELECT 1
-							FROM event_summary_totals
-							WHERE month = v_month
-							  AND log_event_type_id = v_event_type_id
-						) 
-					THEN
-						UPDATE event_summary_totals
-						SET number_of_events = number_of_events + v_num_events
-						WHERE month = v_month
-						  AND log_event_type_id = v_event_type_id;
-					ELSE
-						INSERT INTO event_summary_totals (
-							month, log_event_type_id, number_of_events, record_count
-						)
-						VALUES (v_month, v_event_type_id, v_num_events,0);
-					END IF;
 
 					-- Update the record_count for 'dr' entity only once
 					IF v_entity_prefix = 'dr' THEN
