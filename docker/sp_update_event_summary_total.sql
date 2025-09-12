68e0e64781a62d3bc4e9c2422b73f5d9de051836
QBai <31238531+qifeng-bai@users.noreply.github.com>
2025-09-12 15:32:42 +1000

    #78 revised event_summary_total

--- docker/sp_update_event_summary_total.sql
+++ docker/sp_update_event_summary_total.sql
@@ -8,7 +8,6 @@ CREATE DEFINER=`logger`@`%` PROCEDURE `batch_process_event_summary_totals`(
     IN p_end_id BIGINT
 )
 BEGIN
-    DECLARE done INT DEFAULT 0;
     DECLARE total_log_events_to_process BIGINT DEFAULT 0;
     -- v_ variables are written into the temporary table
     DECLARE v_month INT;
@@ -17,22 +16,6 @@ BEGIN
     DECLARE v_num_events BIGINT;
     DECLARE v_total_records BIGINT;
 
-    -- output variables for debugging
---     DECLARE current_number_of_events INT DEFAULT 0;
---     DECLARE current_record_count INT DEFAULT 0;
---     DECLARE updated_number_of_events INT DEFAULT 0;
---     DECLARE updated_record_count INT DEFAULT 0;
-
-     -- Declare cur_summary for count events
-     DECLARE cur_summary CURSOR FOR
-         SELECT month, log_event_type_id, num_log_event FROM tmp_log_event_summary;
-     -- Declare cursor for iterating over a temporary database stored the aggregated results for total_records of 'dr' entity only
-     DECLARE cur CURSOR FOR
-		SELECT month, log_event_type_id,entity_prefix, num_log_event, total_record_count
-		FROM tmp_aggregated_results;
-
-
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
     -- Drop temporary table if it already exists
 	DROP TEMPORARY TABLE IF EXISTS tmp_log_event_summary;
     DROP TEMPORARY TABLE IF EXISTS tmp_aggregated_results;
@@ -60,35 +43,38 @@ BEGIN
             )
         GROUP BY le.month, le.log_event_type_id
         ORDER BY le.month, le.log_event_type_id;
-
-    OPEN cur_summary;
-
-    read_loop: LOOP
-        SET done = 0;
-        FETCH cur_summary INTO v_month, v_event_type_id, v_num_events;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-             IF EXISTS (
-                    SELECT 1
-                    FROM event_summary_totals
-                    WHERE month = v_month
-                      AND log_event_type_id = v_event_type_id
-                )
-            THEN
-                UPDATE event_summary_totals
-                SET number_of_events = number_of_events + v_num_events
-                WHERE month = v_month
-                  AND log_event_type_id = v_event_type_id;
-            ELSE
-                INSERT INTO event_summary_totals (
-                    month, log_event_type_id, number_of_events, record_count
-                )
-                VALUES (v_month, v_event_type_id, v_num_events,0);
-            END IF;
-
-    END LOOP;
-    CLOSE cur_summary;
+        BEGIN
+            DECLARE done_summary INT DEFAULT 0;
+            DECLARE cur_summary CURSOR FOR
+                SELECT month, log_event_type_id, num_log_event FROM tmp_log_event_summary;
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done_summary = 1;
+            OPEN cur_summary;
+                read_loop: LOOP
+                    SET done_summary = 0;
+                    FETCH cur_summary INTO v_month, v_event_type_id, v_num_events;
+                    IF done_summary THEN
+                        LEAVE read_loop;
+                    END IF;
+                         IF EXISTS (
+                                SELECT 1
+                                FROM event_summary_totals
+                                WHERE month = v_month
+                                  AND log_event_type_id = v_event_type_id
+                            )
+                        THEN
+                            UPDATE event_summary_totals
+                            SET number_of_events = number_of_events + v_num_events
+                            WHERE month = v_month
+                              AND log_event_type_id = v_event_type_id;
+                        ELSE
+                            INSERT INTO event_summary_totals (
+                                month, log_event_type_id, number_of_events, record_count
+                            )
+                            VALUES (v_month, v_event_type_id, v_num_events,0);
+                        END IF;
+                 END LOOP;
+            CLOSE cur_summary;
+        END;
     -- END: Count log_event (excludes those without log_details) based on month and event type(e.g 1002)
 
     -- 2. START
@@ -115,36 +101,36 @@ BEGIN
 		ORDER BY le.log_event_type_id, le.month;
 
 		-- 3. Open cursor and loop through each row
-		OPEN cur;
+		BEGIN
+		    DECLARE done INT DEFAULT 0;
+            DECLARE cur CURSOR FOR
+                SELECT month, log_event_type_id,entity_prefix, num_log_event, total_record_count
+                FROM tmp_aggregated_results;
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+            OPEN cur;
+                read_loop: LOOP
+                        FETCH cur INTO v_month, v_event_type_id, v_entity_prefix, v_num_events, v_total_records;
+                        IF done THEN
+                            LEAVE read_loop;
+                        END IF;
 
-		read_loop: LOOP
-				SET done = 0; -- IMPORTANT: reset done flag for each loop, otherwise it will stop after the first iteration
-				FETCH cur INTO v_month, v_event_type_id, v_entity_prefix, v_num_events, v_total_records;
-				IF done THEN
-					LEAVE read_loop;
-				END IF;
+                                -- todo: need to be clarified
+                                -- The current 'trigger' method updates the number of records for 'dr' entity twice
+                                -- We believe it intention to  update the number of records of 'dr' ONLY
+                                -- Check line 110 as below in the trigger code
+                                --                      IF NEW.entity_uid LIKE 'dr%' THEN
+                                --                             UPDATE event_summary_totals est SET record_count = record_count + NEW.record_count
+                                --                      WHERE est.month = new_month AND est.log_event_type_id = new_log_event_type_id;
+                                --                        END IF;
 
-						-- todo: need to be clarified
-						-- The current 'trigger' method updates the number of records for 'dr' entity twice
-						-- We believe it intention to  update the number of records of 'dr' ONLY
-						-- Check line 110 as below in the trigger code
-						--                      IF NEW.entity_uid LIKE 'dr%' THEN
-						--                             UPDATE event_summary_totals est SET record_count = record_count + NEW.record_count
-						--                      WHERE est.month = new_month AND est.log_event_type_id = new_log_event_type_id;
-						--                        END IF;
-
-					-- Update the record_count for 'dr' entity only once
-					IF v_entity_prefix = 'dr' THEN
-						UPDATE event_summary_totals est SET record_count = record_count + v_total_records
-							WHERE est.month = v_month AND est.log_event_type_id = v_event_type_id;
-					END IF;
-                END
-			LOOP;
-CLOSE cur;
-
--- drop temporary table at the end
-DROP TEMPORARY TABLE IF EXISTS tmp_aggregated_results;
-
-SELECT "COMPLETED: event_summary_breakdown_total", p_start_id, p_end_id;
-
+                            -- Update the record_count for 'dr' entity only once
+                            IF v_entity_prefix = 'dr' THEN
+                                UPDATE event_summary_totals est SET record_count = record_count + v_total_records
+                                    WHERE est.month = v_month AND est.log_event_type_id = v_event_type_id;
+                            END IF;
+                        END LOOP;
+            CLOSE cur;
+            DROP TEMPORARY TABLE IF EXISTS tmp_aggregated_results;
+        END;
+    SELECT "COMPLETED: event_summary_breakdown_total", p_start_id, p_end_id;
 END
