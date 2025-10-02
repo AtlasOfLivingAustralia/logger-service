ec7ecda455f1c051cbc83d94c1a12efa865adb05
QBai <31238531+qifeng-bai@users.noreply.github.com>
2025-10-02 11:42:44 +1000

    #78 minor fixes on the SP

--- grails-app/migrations/sql/V1_sp_update_event_summary_total.sql
+++ grails-app/migrations/sql/V1_sp_update_event_summary_total.sql
@@ -6,7 +6,7 @@
 -- It splits two processes. one for counting number_of_events, another for record_count of 'dr' entity only
 -- They cannot combine into one process
 -- When we count by event_id group by event_type_id, since an event_id may have multiple event_type_id, the count may be duplicated
-DROP PROCEDURE IF EXISTS `logger`.`batch_process_event_summary_totals`;
+DROP PROCEDURE IF EXISTS `logger`.`process_event_summary_totals`;
 CREATE DEFINER=`logger`@`%` PROCEDURE `process_event_summary_totals`(
     IN p_start_id BIGINT,
     IN p_end_id BIGINT
