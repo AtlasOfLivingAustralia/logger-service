ecfbbc27fa65aef338747dfd41cc64a0d7a7d5a2
QBai <31238531+qifeng-bai@users.noreply.github.com>
2025-10-03 10:03:59 +1000

    #78 remove db name in the stored procedures for CICD

--- src/main/resources/sql/V1_sp_process_month_events.sql
+++ src/main/resources/sql/V1_sp_process_month_events.sql
@@ -24,37 +24,37 @@ BEGIN
             WHEN '0' THEN
                 BEGIN
                     DELETE FROM event_summary_totals WHERE month = target_month;
-                    CALL logger.process_event_summary_totals(start_id, end_id);
+                    CALL process_event_summary_totals(start_id, end_id);
 --                     SELECT * FROM event_summary_totals WHERE month = target_month;
                 END;
             WHEN '1' THEN
                 BEGIN
                     DELETE FROM event_summary_breakdown_reason WHERE month = target_month;
-                    CALL logger.process_event_summary_breakdown_reason(start_id, end_id);
+                    CALL process_event_summary_breakdown_reason(start_id, end_id);
 --                     SELECT * FROM event_summary_breakdown_reason WHERE month = target_month;
                 END;
             WHEN '2' THEN
                 BEGIN
                     DELETE FROM event_summary_breakdown_reason_entity WHERE month = target_month;
-                    CALL logger.process_event_summary_breakdown_reason_entity(start_id, end_id);
+                    CALL process_event_summary_breakdown_reason_entity(start_id, end_id);
 --                     SELECT * FROM event_summary_breakdown_reason_entity WHERE month = target_month;
                 END;
             WHEN '3' THEN
                 BEGIN
                     DELETE FROM event_summary_breakdown_reason_entity_source WHERE month = target_month;
-                    CALL logger.process_event_summary_breakdown_reason_entity_source(start_id, end_id);
+                    CALL process_event_summary_breakdown_reason_entity_source(start_id, end_id);
 --                     SELECT * FROM event_summary_breakdown_reason_entity_source WHERE month = target_month;
                 END;
             WHEN '4' THEN
                 BEGIN
                     DELETE FROM event_summary_breakdown_email WHERE month = target_month;
-                    CALL logger.process_event_summary_breakdown_email(start_id, end_id);
+                    CALL process_event_summary_breakdown_email(start_id, end_id);
 --                     SELECT * FROM event_summary_breakdown_email WHERE month = target_month;
                 END;
             WHEN '5' THEN
                 BEGIN
                     DELETE FROM event_summary_breakdown_email_entity WHERE month = target_month;
-                    CALL logger.process_event_summary_breakdown_email_entity(start_id, end_id);
+                    CALL process_event_summary_breakdown_email_entity(start_id, end_id);
 --                     SELECT * FROM event_summary_breakdown_email_entity WHERE month = target_month;
                 END;
             ELSE
